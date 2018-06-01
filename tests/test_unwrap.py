b1206f60ed473715e8564ecdd9031ecace017188
ftorto <dev.torto@gmail.com>
2018-06-01 12:06:10 +0200

    [#173032] updating import path
    Signed-off-by: ftorto <dev.torto@gmail.com>

--- tests/test_unwrap.py
+++ tests/test_unwrap.py
@@ -23,12 +23,12 @@ import numpy as np
 
 from numpy import pi as pi
 
-from ikats.algo.core.unwrap import unwrap_tsuid
-from ikats.algo.core.unwrap.unwrap import unwrap_ts_list, TSUnit
+from ikats.algo.op-unwrap import unwrap_tsuid
+from ikats.algo.op-unwrap.unwrap import unwrap_ts_list, TSUnit
 from ikats.core.resource.api import IkatsApi
 
 # Logger definition
-LOGGER = logging.getLogger('ikats.algo.core.unwrap.unwrap')
+LOGGER = logging.getLogger('ikats.algo.op-unwrap.unwrap')
 # Log format
 LOGGER.setLevel(logging.DEBUG)
 FORMATTER = logging.Formatter('%(asctime)s:%(levelname)s:%(funcName)s:%(message)s')
